0115663f8fb331881a4c3e380584e6fc92b2712e
Folkert van Heusden <mail@vanheusden.com>
2022-08-17 09:01:17 +0200

    max iterations can now also be infinite

--- monkeycoder.py
+++ monkeycoder.py
@@ -7,16 +7,16 @@ targets  = [
             { 'initial_values': [ { 'width' : 8, 'value' : 1 },
                                   { 'width' : 8, 'value' : 1 } ],
               'result_acc': 2 },
-            { 'initial_values': [ { 'width' : 8, 'value' : 2 },
-                                  { 'width' : 8, 'value' : 1 } ],
-              'result_acc': 3 },
-            { 'initial_values': [ { 'width' : 8, 'value' : 1 },
-                                  { 'width' : 8, 'value' : 2 } ],
-              'result_acc': 3 },
+            { 'initial_values': [ { 'width' : 8, 'value' : 32 },
+                                  { 'width' : 8, 'value' : 16 } ],
+              'result_acc': 48 },
+            { 'initial_values': [ { 'width' : 8, 'value' : 16 },
+                                  { 'width' : 8, 'value' : 32 } ],
+              'result_acc': 48 },
     ]
 
-max_program_iterations = 30000
-max_program_length     = 1024
+max_program_iterations = None
+max_program_length     = 256
 
 iterations = 0
 
@@ -27,10 +27,12 @@ best_program    = None
 best_iterations = None
 first_output    = True
 
-targets_ok_stat = 0
-targets_ok_n    = 0
+targets_ok_stat  = 0
+targets_ok_n     = 0
+targets_ok_bestn = 0
+targets_ok_best  = None
 
-while iterations < max_program_iterations:
+while max_program_iterations == None or iterations < max_program_iterations:
     ok = True
 
     iterations += 1
@@ -58,6 +60,10 @@ while iterations < max_program_iterations:
     targets_ok_stat += n_targets_ok
     targets_ok_n    += 1
 
+    if n_targets_ok > targets_ok_bestn:
+        targets_ok_bestn = n_targets_ok
+        targets_ok_best  = program
+
     if ok and (best_program == None or len(program) < len(best_program)):
         best_program    = program
         p.insert_program_init(best_program, target['initial_values'])
@@ -70,12 +76,17 @@ while iterations < max_program_iterations:
             print()
             print(f'First output after {iterations} iterations ({time.time() - start_ts:.2f} seconds)')
 
+        if max_program_iterations == None:
+            break
+
     now = time.time()
 
     if now - prev_ts >= 2:
         prev_ts = now
 
-        print(f'Iterations done: {iterations}, average n_ok: {targets_ok_stat / targets_ok_n:.4f}, run time: {now - start_ts:.2f} seconds\r', end='')
+        diff_ts = now - start_ts
+
+        print(f'Iterations done: {iterations}, average n_ok: {targets_ok_stat / targets_ok_n:.4f}[{targets_ok_bestn}], run time: {now - start_ts:.2f} seconds, {iterations / diff_ts:.2f} iterations per second\r', end='')
 
 # TODO: remove random instructions and check if it still works
 
